8123f7ed01b9524f88ae1ff647ab976db05f9b35
John Goh <john@accendo.com.my>
2018-10-01 14:41:22 +0800

    test routing 4

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 console.log('sup')
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('scripts loaded!')
+    console.log('document ready scripts loaded!')
     $(".root-container").load("assets/pages/main.html");
-    $('#main').on('click', () => {
-        $(".root-container").load("assets/pages/main.html");
-        event.preventDefault();
-        console.log('main')
-    });
-    $('#aboutus').on('click', () => {
-        $(".the-content").load("assets/pages/aboutus.html");
-        event.preventDefault();
-        console.log('aboutus')
-        
-    });
-    $('#portfolio').on('click', () => {
-        $(".the-content").load("assets/pages/portfolio.html");
-        event.preventDefault();
-        console.log('portfolio')
-    });
+    $(".root-container").addEventListener("DOMContentLoaded", (event) => {
+        console.log('routing scripts loaded!')
+        $('#main').on('click', () => {
+            $(".root-container").load("assets/pages/main.html");
+            event.preventDefault();
+            console.log('main')
+        });
+        $('#aboutus').on('click', () => {
+            $(".the-content").load("assets/pages/aboutus.html");
+            event.preventDefault();
+            console.log('aboutus')
+            
+        });
+        $('#portfolio').on('click', () => {
+            $(".the-content").load("assets/pages/portfolio.html");
+            event.preventDefault();
+            console.log('portfolio')
+        });
+    })
 });
 
 const test = () => {
