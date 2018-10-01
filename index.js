1c0eada9a60694a30eefe67b289a2623a36a4b11
John Goh <john@accendo.com.my>
2018-10-01 14:35:29 +0800

    test routing 2.

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $(".root-container").load("assets/pages/main.html");
     $('#main').on('click', () => {
         $(".root-container").load("assets/pages/main.html");
+        event.preventDefault();
+        console.log('main')
     });
     $('#aboutus').on('click', () => {
-        $(".the-content").load("assets/pages/main.html");
-
+        $(".the-content").load("assets/pages/aboutus.html");
+        event.preventDefault();
+        console.log('aboutus')
+        
     });
     $('#portfolio').on('click', () => {
-        $(".the-content").load("assets/pages/main.html");
+        $(".the-content").load("assets/pages/portfolio.html");
+        event.preventDefault();
+        console.log('portfolio')
     });
 });
 
