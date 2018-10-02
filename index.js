9b5ff1fed363f623e402965848d6aaf2eeab6a8b
John Goh <john@accendo.com.my>
2018-10-02 17:05:10 +0800

    fix routing issue

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 console.log('sup')
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('document ready scripts loaded!')
-    $(".root-container").load("assets/pages/main.html");
-    $(".root-container").ready(() => {
-        loadRoutes()
-    })
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     console.log('document ready scripts loaded!')
+//     $(".root-container").load("assets/pages/main.html");
+//     $(".root-container").ready(() => {
+//         loadRoutes()
+//     })
+// });
 
-const loadRoutes = () => {
-    console.log('routing scripts loaded!')
-    $('#main').on('click', () => {
-        $(".root-container").load("assets/pages/main.html");
-        setTimeout(loadRoutes, 200)
-        event.preventDefault();
-        console.log('main')
-    });
-    $('#aboutus').on('click', () => {
-        $(".the-content").load("assets/pages/aboutus.html");
-        event.preventDefault();
-        console.log('aboutus')
+// const loadRoutes = () => {
+//     console.log('routing scripts loaded!')
+//     $('#main').on('click', () => {
+//         $(".root-container").load("assets/pages/main.html");
+//         setTimeout(loadRoutes, 200)
+//         event.preventDefault();
+//         console.log('main')
+//     });
+//     $('#aboutus').on('click', () => {
+//         $(".the-content").load("assets/pages/aboutus.html");
+//         event.preventDefault();
+//         console.log('aboutus')
         
-    });
-    $('#portfolio').on('click', () => {
-        $(".the-content").load("assets/pages/portfolio.html");
-        event.preventDefault();
-        console.log('portfolio')
-    });
-}
+//     });
+//     $('#portfolio').on('click', () => {
+//         $(".the-content").load("assets/pages/portfolio.html");
+//         event.preventDefault();
+//         console.log('portfolio')
+//     });
+// }
